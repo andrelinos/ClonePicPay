fb15cfc9cc7b801b35c864cdc11050cf951df101
Andrelino Silva <andrelinocom@gmail.com>
2020-05-01 00:08:18 -0300

    Criando Clone PicPay

--- src/screens/Wallet/index.js
+++ src/screens/Wallet/index.js
@@ -1,10 +1,131 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Switch } from 'react-native';
+import { Feather, FontAwesome, Ionicons } from '@expo/vector-icons';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContainer,
+  Title,
+  BalanceContainer,
+  Value,
+  Bold,
+  BoldHide,
+  EyeButton,
+  Info,
+  Actions,
+  Action,
+  ActionLabel,
+  UseBalance,
+  UseBalanceTitle,
+  PaymentMethods,
+  PaymentsMethodsTitle,
+  Card,
+  CardDetails,
+  CardTitle,
+  CardDescription,
+  CardAddContainer,
+  CardAddIcon,
+  CardAddLabel,
+  CardImg,
 
-export default function Wallet() {
+} from './styles';
+
+import creditCard from '../../assets/credit-card.png';
+
+export default function Wallet(props) {
+  const [isValueVisible, setIsValueVisible] = useState(true)
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  console.log(isValueVisible);
+
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
-    <Text>Carteira</Text>
+    <Container>
+      <Header colors={isEnabled ? ['#52e78c', '#1ab563'] : ['#a4b0be', '#747d8c'] }>
+        <HeaderContainer>
+          <Title>Saldo PicPay</Title>
+          <BalanceContainer>
+
+            <Value>
+            R$
+            </Value>
+            {isValueVisible
+                  ? <Bold>0,00</Bold>
+                  : <BoldHide>
+                    </BoldHide>
+                }
+            <EyeButton
+              onPress={() => setIsValueVisible(isValueVisible => !isValueVisible)}
+            >
+             {isValueVisible
+             ? <Feather name="eye" size={28} color="#fff" />
+             : <Feather name="eye-off" size={28} color="#fff" />
+             }
+            </EyeButton>
+          </BalanceContainer>
+
+          <Info>
+            Seu saldo está rendendo 100% do CDI.
+          </Info>
+
+          <Actions>
+            <Action>
+              <Ionicons name="ios-cash" size={22} color="#fff" />
+              <ActionLabel>Adicionar</ActionLabel>
+            </Action>
+            <Action>
+              <FontAwesome name="bank" size={22} color="#fff" />
+              <ActionLabel>Adicionar</ActionLabel>
+            </Action>
+          </Actions>
+        </HeaderContainer>
+      </Header>
+
+      <UseBalance>
+        <UseBalanceTitle>
+          Usar saldo ao pagar
+        </UseBalanceTitle>
+
+        <Switch
+          trackColor={{ false: "#767577", true: "#9ef7ba" }}
+          thumbColor={isEnabled ? "#1ab563" : "#ddd"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </UseBalance>
+
+      <PaymentMethods>
+        <PaymentsMethodsTitle>
+          Formas de pagamento
+        </PaymentsMethodsTitle>
+
+        <Card>
+          <CardDetails>
+            <CardTitle>
+              Cadastre seu cartão de crédito
+            </CardTitle>
+            <CardDescription>
+              Cadastre um cartão de crédito para poder fazer pagamentos mesmo quando não tiver saldo no seu PicPay.
+            </CardDescription>
+
+            <CardAddContainer>
+              <CardAddIcon>
+                <Ionicons name="ios-add" size={42} color="#1ab563" />
+              </CardAddIcon>
+              <CardAddLabel>
+                Adicionar cartão de crédito
+              </CardAddLabel>
+            </CardAddContainer>
+
+          </CardDetails>
+          <CardImg source={creditCard} resizeMode="contain" />
+        </Card>
+
+
+      </PaymentMethods>
+    </Container>
   );
 }
